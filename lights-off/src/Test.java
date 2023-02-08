391d5eaf9f3dd74add34514a3e5fe6c7e4b50c6a
mohammad afzalzadeh <76066524+MohammadAfzalzadeh@users.noreply.github.com>
2023-02-08 22:22:34 +0330

    test BFS

--- lights-off/src/Test.java
+++ lights-off/src/Test.java
@@ -2,12 +2,16 @@ public class Test {
     public static void main(String[] args) {
         Board board = new Board(5);
         board.printBoard();
-        for (int i = 0; i < board.getSize(); i++) {
-            for (int j = 0; j < board.getSize(); j++) {
-                board.clickTile(i,j);
-            }
-        }
-        board.printBoard();
+        // for (int i = 0; i < board.getSize(); i++) {
+        //     for (int j = 0; j < board.getSize(); j++) {
+        //         board.clickTile(i,j);
+        //     }
+        // }
+
+        //BFS f = new BFS();
+        new BFS().search(new Node (null , -1 , -1 , board));
+
+//        board.printBoard();
 
     }
 }
